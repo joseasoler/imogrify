056fe93bf5620a9b70f82226a0c66a5e6cc2cff2
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2024-10-23 04:35:12 +0200

    Improve raw image unit testing.

--- tests/image/raw_image_test.cpp
+++ tests/image/raw_image_test.cpp
@@ -17,6 +17,25 @@
 using imfy::image::image_size;
 using imfy::image::raw_image;
 
+namespace
+{
+
+constexpr image_size default_size{18U, 19U};
+
+template <std::uint8_t Channels, std::uint8_t BitSize>
+bool test_modulo_raw_image()
+{
+	const raw_image<Channels, BitSize> image(default_size, imfy::image::initialization::modulo);
+	const auto data = image.data();
+	bool result = true;
+	for (std::size_t index = 0U; result && index < (data.size() - raw_image<Channels, BitSize>::pixel_size); ++index)
+	{
+		result = data[index] != data[index + raw_image<Channels, BitSize>::pixel_size];
+	}
+	return result;
+}
+} // Anonymous namespace
+
 TEST_CASE("Raw image type checks")
 {
 	static_assert(std::is_same_v<raw_image<1U, 8U>::value_type, std::uint8_t>);
@@ -25,36 +44,36 @@ TEST_CASE("Raw image type checks")
 	static_assert(std::is_same_v<raw_image<2U, 16U>::value_type, std::uint16_t>);
 }
 
-static constexpr image_size size{18U, 19U};
-
 TEST_CASE("Uninitialized raw image")
 {
-	const raw_image<4U, 8U> image_8(size);
-	CHECK(image_8.width() == size.width);
-	CHECK(image_8.height() == size.height);
-	CHECK(image_8.data().size() == static_cast<std::size_t>(size.width) * size.height * raw_image<4U, 8U>::pixel_size);
+	const raw_image<4U, 8U> image_8(default_size);
+	CHECK(image_8.width() == default_size.width);
+	CHECK(image_8.height() == default_size.height);
+	CHECK(
+			image_8.data().size() ==
+			static_cast<std::size_t>(default_size.width) * default_size.height * raw_image<4U, 8U>::pixel_size
+	);
 
-	const raw_image<2U, 8U> image_16(size);
-	CHECK(image_16.width() == size.width);
-	CHECK(image_16.height() == size.height);
-	CHECK(image_16.data().size() == static_cast<std::size_t>(size.width) * size.height * raw_image<2U, 8U>::pixel_size);
+	const raw_image<2U, 8U> image_16(default_size);
+	CHECK(image_16.width() == default_size.width);
+	CHECK(image_16.height() == default_size.height);
+	CHECK(
+			image_16.data().size() ==
+			static_cast<std::size_t>(default_size.width) * default_size.height * raw_image<2U, 8U>::pixel_size
+	);
 }
 
 TEST_CASE("Zeroed raw image")
 {
-	const raw_image<4U, 16U> image(size, imfy::image::initialization::zero);
+	const raw_image<4U, 16U> image(default_size, imfy::image::initialization::zero);
 	const auto data = image.data();
 	CHECK(std::all_of(data.begin(), data.end(), [](std::uint16_t value) -> bool { return value == 0U; }));
 }
 
 TEST_CASE("Modulo raw image")
 {
-	const raw_image<3U, 8U> image(size, imfy::image::initialization::modulo);
-	const auto data = image.data();
-	bool result = true;
-	for (std::size_t index = 0U; result && index < (data.size() - raw_image<3U, 8U>::pixel_size); ++index)
-	{
-		result = data[index] != data[index + raw_image<3U, 8U>::pixel_size];
-	}
-	CHECK(result);
+	CHECK(test_modulo_raw_image<1U, 8U>());
+	CHECK(test_modulo_raw_image<2U, 16U>());
+	CHECK(test_modulo_raw_image<3U, 8U>());
+	CHECK(test_modulo_raw_image<4U, 16U>());
 }
