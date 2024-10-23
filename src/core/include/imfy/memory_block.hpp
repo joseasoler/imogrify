7b8a6d56ff4280187579c5cfc26e8dab9cca176f
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2024-10-23 04:29:28 +0200

    memory_block now relies on std::unique_ptr.

--- src/core/include/imfy/memory_block.hpp
+++ src/core/include/imfy/memory_block.hpp
@@ -9,12 +9,30 @@
 #include <imfy/concepts.hpp>
 
 #include <cstddef>
+#include <memory>
 #include <new>
 #include <span>
 #include <type_traits>
 
 namespace imfy
 {
+
+namespace detail
+{
+
+template <pod_type Type, standard_empty_allocator Allocator>
+struct memory_block_delete final
+{
+	explicit memory_block_delete(std::size_t blck_size) noexcept
+		: block_size{blck_size}
+	{
+	}
+	void operator()(Type* pointer) const { Allocator{}.deallocate(pointer, block_size); }
+
+	std::size_t block_size;
+};
+
+} // namespace detail
 
 /**
  * Memory block with managed allocation and de-allocation.
@@ -30,8 +48,7 @@ public:
 	using size_type = typename Allocator::size_type;
 
 	explicit memory_block(size_type block_size)
-		: block_{Allocator{}.allocate(block_size)}
-		, size_{block_size}
+		: block_{Allocator{}.allocate(block_size), detail::memory_block_delete<value_type, Allocator>{block_size}}
 	{
 	}
 	memory_block() = delete;
@@ -39,16 +56,14 @@ public:
 	memory_block(memory_block&&) noexcept = default;
 	memory_block& operator=(const memory_block&) = delete;
 	memory_block& operator=(memory_block&&) noexcept = default;
+	~memory_block() = default;
 
-	~memory_block() { Allocator{}.deallocate(block_, size_); }
-
-	[[nodiscard]] size_type size() const noexcept { return size_; }
-	[[nodiscard]] std::span<value_type> span() noexcept { return {block_, size_}; }
-	[[nodiscard]] std::span<const value_type> span() const noexcept { return {block_, size_}; }
+	[[nodiscard]] size_type size() const noexcept { return block_.get_deleter().block_size; }
+	[[nodiscard]] std::span<value_type> span() noexcept { return {block_.get(), size()}; }
+	[[nodiscard]] std::span<const value_type> span() const noexcept { return {block_.get(), size()}; }
 
 private:
-	value_type* block_;
-	size_type size_;
+	std::unique_ptr<value_type, detail::memory_block_delete<value_type, Allocator>> block_;
 };
 
 } // namespace imfy
