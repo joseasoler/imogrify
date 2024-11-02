ad9d83e4ed104ecafda35d861aba7134dafcba1b
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2024-11-02 15:54:29 +0100

    vcpkg triplet file for windows-msvc.

---
+++ cmake/custom_vcpkg/triplets/imfy-x64-windows-msvc.cmake
@@ -0,0 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_LOAD_VCVARS_ENV ON)
+set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_C_FLAGS "/arch:AVX2")
+set(VCPKG_CXX_FLAGS "${VCPKG_C_FLAGS}")
